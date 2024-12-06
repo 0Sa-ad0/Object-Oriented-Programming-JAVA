28d648c47fb51f3091d1a009ca6a5dc421d5ed41
SAAD <96654865+0Sa-ad0@users.noreply.github.com>
2024-12-06 19:41:56 +0600

    ok

--- AUTOMATED-FOOD-BEVERAGE-SERVICE-main/AIUB Automated Food and Braverage Service/AIUB Automated Food and Braverage Service/interfaces/IStudents.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-import classes.*;
-public interface IStudents
-{
-	void insertStudent(Student s);
-	void removeStudent(Student s);
-	Student getStudent(String id);
-	void showAllStudents();
-}
