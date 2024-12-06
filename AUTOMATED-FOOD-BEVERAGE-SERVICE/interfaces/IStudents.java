b50ee04034c6d79d6639531e186fa019133eb867
SAAD <96654865+0Sa-ad0@users.noreply.github.com>
2024-12-06 20:24:08 +0600

    ok

--- AUTOMATED-FOOD-BEVERAGE-SERVICE/interfaces/IStudents.java
+++ AUTOMATED-FOOD-BEVERAGE-SERVICE/interfaces/IStudents.java
@@ -1,9 +1,14 @@
 package interfaces;
+
 import classes.*;
-public interface IStudents
-{
-	void insertStudent(Student s);
-	void removeStudent(Student s);
-	Student getStudent(String id);
-	void showAllStudents();
+
+public interface IStudents {
+
+    void insertStudent(Student s);
+
+    void removeStudent(Student s);
+
+    Student getStudent(String id);
+
+    void showAllStudents();
 }
--- AUTOMATED-FOOD-BEVERAGE-SERVICE/interfaces/ITransactions.java
+++ AUTOMATED-FOOD-BEVERAGE-SERVICE/interfaces/ITransactions.java
@@ -1,8 +1,12 @@
 package interfaces;
+
 import java.lang.*;
 import classes.*;
-public interface ITransactions		//common example of an interface
+
+public interface ITransactions //common example of an interface
 {
-	void buyAnItem(int amount);			//we can just mention/declare methods
-	void addMoney(int amount);
+
+    void buyAnItem(int amount);			//we can just mention/declare methods
+
+    void addMoney(int amount);
 }
